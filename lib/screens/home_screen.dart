61c2a9f4e64c90b292744c98c0e55279736d25aa
mfouques <mathis.fouques1@etu.univ-lorraine.fr>
2019-10-17 11:15:38 +0200

    Added presentation of home screen.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
 
+import 'package:forum_horizon_chimie/app_localizations.dart';
+import 'package:forum_horizon_chimie/colors.dart';
+import 'package:forum_horizon_chimie/widgets/grid_tile_classic.dart';
+import 'package:forum_horizon_chimie/widgets/page_title_classic.dart';
+
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Theme.of(context).primaryColor,
+      child: ListView(
+        children: <Widget>[
+          ClassicPageTitle(
+            title: "home",
+          ),
+          GridView.count(
+            padding: EdgeInsets.all(20),
+            shrinkWrap: true,
+            crossAxisCount: 2,
+            crossAxisSpacing: 15,
+            mainAxisSpacing: 15,
+            children: <Widget>[
+              GridTileClassic(colorTile: darkBlueColor, textCode: "home",),
+              GridTileClassic(colorTile: simpleBlueColor, textCode: "home",),
+              GridTileClassic(colorTile: lightBlueColor, textCode: "home",),
+              GridTileClassic(colorTile: lightGreenColor, textCode: "home",),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ lib/widgets/grid_tile_classic.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:forum_horizon_chimie/app_localizations.dart';
+
+class GridTileClassic extends StatelessWidget {
+  final Color colorTile;
+  final String textCode;
+
+  GridTileClassic({@required this.colorTile, @required this.textCode});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(15),
+      child: FittedBox(
+        alignment: Alignment.bottomRight,
+        child: Text(
+          AppLocalizations.of(context).translate(textCode),
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: 'Gotham',
+          ),
+        ),
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        shape: BoxShape.rectangle,
+        color: colorTile,
+        gradient: LinearGradient(
+          colors: <Color>[
+            colorTile,
+            Colors.white
+          ],
+          begin: Alignment.bottomRight,
+          end: Alignment.topLeft,
+        ),
+        boxShadow: <BoxShadow>[
+          BoxShadow(color: colorTile, blurRadius: 6, spreadRadius: 0),
+        ]
+      ),
+    );
+  }
+}
